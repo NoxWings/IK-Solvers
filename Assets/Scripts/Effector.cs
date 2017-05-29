8c3f786e536dbfe6d6b0dd2e8cd2770efe21278f
David Garcia Miguel <noxwings@gmail.com>
2017-05-30 01:33:49 +0200

    Some effector boilerplate code

---
+++ Assets/Scripts/Effector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effector : MonoBehaviour {
+    #region public variables
+    public Transform ikTarget = null;
+    public int chainLength = 2;
+    #endregion
+
+    private List<Bone> bones = new List<Bone>();
+    private float maxLength = 0;
+
+    void Start() {
+        this.bones.Clear();
+        Transform current = this.transform;
+
+        for (int i = 0; i < chainLength; i++) {
+            if (current.parent == null)
+                break;
+
+            Bone b = new Bone(current.parent, current);
+            this.maxLength += b.vector.magnitude;
+            this.bones.Add(b);
+
+            current = current.parent;
+        }
+        this.bones.Reverse();
+    }
+
+    void Update () {
+        if (IsTargetWithinMaxLength())
+            FabrikSolver();
+        else
+            SimpleSolver();
+    }
+
+    private bool IsTargetWithinMaxLength() {
+        Vector3 startPos = this.bones[0].transform.position;
+        Vector3 endPos = ikTarget.position;
+        float targetDistance = Vector3.Distance(startPos, endPos);
+        return (targetDistance <= maxLength);
+    }
+
+    private void FabrikSolver() {
+    }
+
+    private void SimpleSolver() {
+    }
+}
