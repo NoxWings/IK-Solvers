841a6da5beb4ec7faa4c6ad9378fdee6ad82ed62
David Garcia Miguel <noxwings@gmail.com>
2017-05-30 01:32:29 +0200

    Model bone class

---
+++ Assets/Scripts/Models/Bone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bone {
+    #region public properties
+    public Transform transform { get; private set; }
+    public Vector3 vector { get; private set; }
+    #endregion
+
+    public Bone(Transform boneInit, Transform boneEnd) {
+        this.transform = boneInit;
+        this.vector = boneEnd.position - boneInit.position;
+    }
+}
